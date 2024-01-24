7514c0a9806b4cb901b0fe3956122ebd21ec5ac4
Ayush kumar <ayushverma8900@gmail.com>
2024-01-24 23:01:39 +0530

    confusion_matrix

--- dataset_image_view.py
+++ dataset_image_view.py
@@ -20,3 +20,28 @@ def view_random_image(target_dir,target_class):
 
   print(f"image shape:{img.shape}")
   return img
+
+
+
+
+# let's see by passing random images through it
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import random
+
+target_class = random.choice(train_data_1.class_names)
+target_dir = "10_food_classes_1_percent/train"+"/"+target_class
+random_image = random.choice(os.listdir(target_dir))
+random_img_path = target_dir+"/"+random_image
+img = mpimg.imread(random_img_path)
+plt.figure(figsize=(10,10))
+plt.subplot(1,2,1)
+plt.imshow(img)
+plt.axis(False)
+plt.title(f"orginal_image of {target_class}")
+img = data_augmentation(tf.expand_dims(img,axis=0))
+plt.subplot(1,2,2)
+plt.imshow(tf.squeeze(img)/255.)
+plt.axis(False)
+plt.title(f"augmented image of {target_class}")
